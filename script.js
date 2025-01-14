9cd42b9d1f969ee8875ac9d0535e07883a3be7b4
unknown <andresfcastaneda@yahoo.com>
2025-01-13 22:33:43 -0300

    amaca001

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 5000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
